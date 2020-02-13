d399fa18dc5197f567a1fadbefad585392ee37ee
ndzamela <ndzamela@users.noreply.github.com>
2020-02-13 08:37:15 +0200

    Code file created.

---
+++ code.py
@@ -0,0 +1,24 @@
+# --------------
+#  Read the data of the format .yaml type
+import json
+
+
+# using with open command to read the file
+
+
+#  Women who got the first Nobel Prize?
+#  How many have come from india?
+#  Calculate category wise number of prizes for the people who came from India?
+#   Which country has produced the highest number of Nobel winners for category `Chemistry`?
+from collections import Counter
+import operator
+
+
+
+#  Which Organization won the most nobel prizes in the category "Physics" and "Chemistry" ?
+
+#  What was the Motivation for awarding the Nobel Prize for Marie Curie, nÃ©e Sklodowska?
+
+#  In which category people got Noble Prize in the year 1994?
+
+
